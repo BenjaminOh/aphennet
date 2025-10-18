aa80b160d7d91c076dc9084ed69a718543c9e74e
sol772 <53934919+sol772@users.noreply.github.com>
2025-10-18 17:45:30 +0900

    feat:갤러리게시판 목록 페이지 수정

--- fe/src/components/user/board/-components/GalleryList.tsx
+++ fe/src/components/user/board/-components/GalleryList.tsx
@@ -5,19 +5,26 @@ import { API_URL } from "@/config/apiConfig";
 
 import { PostItem } from "../PostList";
 
-export default function GalleryList({ items, category }: { items: PostItem[]; category: string }) {
+export default function Gallery2List({
+    items,
+    category,
+    handlePostClick,
+}: {
+    items: PostItem[];
+    category: string;
+    handlePostClick: (item: PostItem, e: React.MouseEvent) => void;
+}) {
     return (
-        <ul className="flex flex-col gap-[24px] md:gap-[40px] xl:flex-row xl:flex-wrap xl:gap-[60px]">
+        <ul className="flex flex-col gap-[24px] md:flex-row md:flex-wrap xl:gap-x-[20px] xl:gap-y-[40px]">
             {items.map((item, i) => (
-                <li key={`post_${i}`} className="xl:w-[calc(50%-30px)]">
+                <li key={`post_${i}`} className="md:w-[calc(50%-12px)] xl:w-[calc(25%-15px)]">
                     <Link
                         href={`/${category}/${item.idx}`}
-                        className="group flex flex-col gap-[16px] md:flex-row md:gap-[24px]"
+                        className="group flex flex-col gap-[12px]"
+                        onClick={e => handlePostClick(item, e)}
                     >
-                        <div className="relative h-0 w-full overflow-hidden pb-[133.333%] transition-all duration-300 after:absolute after:inset-0 after:h-full after:w-full after:rounded-[20px] after:border-2 after:border-[#012E69] after:opacity-0 after:transition-all after:duration-300 after:content-[''] md:w-[41.666%] md:pb-[55.555%] xl:group-hover:rounded-[20px] xl:group-hover:after:opacity-100">
-                            {item.b_secret === "Y" ? (
-                                <NoImage />
-                            ) : item.b_img ? (
+                        <div className="relative h-0 w-full overflow-hidden rounded-[20px] pb-[62.5%] transition-all duration-300 after:absolute after:inset-0 after:h-full after:w-full after:bg-[linear-gradient(180deg,rgba(0,0,0,0)_0%,rgba(0,0,0,0.4)_100%)] after:opacity-100 after:transition-all after:duration-300 after:content-[''] xl:group-hover:after:opacity-0">
+                            {item.b_img ? (
                                 <img
                                     src={`${API_URL}/${item.b_img}`}
                                     alt="이미지"
@@ -33,27 +40,11 @@ export default function GalleryList({ items, category }: { items: PostItem[]; ca
                                 <NoImage />
                             )}
                         </div>
-                        <div className="flex flex-col gap-[4px] md:flex-1 md:gap-[12px]">
-                            <div className="flex items-center gap-[8px]">
-                                {item.b_notice === "1" && ""}
-                                <p className="rounded-full bg-[#F2F3F8] p-[4px_16px] text-[14px] font-[500] text-[#666] transition-all xl:group-hover:bg-[#012E69] xl:group-hover:text-white">
-                                    {item.b_reg_date}
-                                </p>
-                            </div>
-                            <div className="flex flex-col gap-[8px] md:flex-1 md:justify-between md:gap-[24px]">
-                                <p className="overflow-hidden text-[18px] font-[700] transition-all [-webkit-box-orient:vertical] [-webkit-line-clamp:2] [display:-webkit-box] md:text-[24px] xl:group-hover:underline">
-                                    {item.b_title}
-                                </p>
-                                <div className="flex flex-col gap-[24px]">
-                                    <p className="overflow-hidden font-[500] text-[#666] [-webkit-box-orient:vertical] [-webkit-line-clamp:2] [display:-webkit-box] md:[-webkit-line-clamp:4]">
-                                        {item.b_contents}
-                                    </p>
-                                    <div className="hidden w-fit items-center gap-[12px] border-b-2 border-[#060606] py-[4px] text-[18px] font-[500] leading-[27px] md:flex">
-                                        <p>더보기</p>
-                                        {/* <Image src={icPlus} alt="더보기" /> */}
-                                    </div>
-                                </div>
-                            </div>
+                        <div>
+                            <p className="overflow-hidden text-[18px] font-[700] transition-all [-webkit-box-orient:vertical] [-webkit-line-clamp:2] [display:-webkit-box] md:text-[20px] xl:group-hover:underline">
+                                {item.b_title}
+                            </p>
+                            <p className="pt-[8px] text-[rgba(102,102,105,0.60)]">{item.b_reg_date}</p>
                         </div>
                     </Link>
                 </li>
